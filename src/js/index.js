9ee7e282867bdc43722e11e71a770cdfa72bb69c
k1k3cb <enriquecbar@gmail.com>
2023-03-13 11:53:53 +0100

    modi redes sociales e inicio objeto trabajos

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,63 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
+const toggleMenuElement = document.getElementById('toggle-menu');
+const menuElement = document.getElementById('menu');
+const hamburgerElement = document.getElementById('hamburger-menu');
 
-const toggleMenuElement = document.getElementById("toggle-menu");
-const menuElement = document.getElementById("menu");
-const hamburgerElement = document.getElementById("hamburger-menu");
+const portfolioWorks = [
+	{
+		image: '/assets/images/password-generator.jpg',
+		title: 'passwordGenerator',
+		description:
+			'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. ',
+		githugLink: ''
+	}
+];
 
+// const addWork = () => {
+// 	portfolioWorks.forEach{
+// 		blabla.contentTex{
+
+// 		}
+// 	};
+// };
 
 function toggleMenu() {
 	// console.log("click click");
-	menuElement.classList.toggle("hidden");
-	hamburgerElement.classList.toggle("hamburger-icon-close");
+	menuElement.classList.toggle('hidden');
+	hamburgerElement.classList.toggle('hamburger-icon-close');
 }
 
-toggleMenuElement.addEventListener("click", toggleMenu);
-
-
-
+toggleMenuElement.addEventListener('click', toggleMenu);
 
 // añadir y eliminar la clase hidden segun tamaño pantalla
 
 const resizeWindow = () => {
-	if (window.matchMedia("(min-width: 1024px)").matches) {
-		menuElement.classList.remove("hidden");
+	if (window.matchMedia('(min-width: 1024px)').matches) {
+		menuElement.classList.remove('hidden');
 	} else {
-		menuElement.classList.add("hidden");
+		menuElement.classList.add('hidden');
 	}
 };
 
 resizeWindow();
 
-window.addEventListener("resize", resizeWindow);
-
-
+window.addEventListener('resize', resizeWindow);
 
 //dark mode
 
-const darkModeButtonElement = document.getElementById("darkmode-btn");
+const darkModeButtonElement = document.getElementById('darkmode-btn');
 
-darkModeButtonElement.addEventListener("click", () => {
+darkModeButtonElement.addEventListener('click', () => {
 	// console.log("click click");
-	document.body.classList.toggle("dark-mode");
-	darkModeButtonElement.classList.toggle("darkmode-icon--white");
+	document.body.classList.toggle('dark-mode');
+	darkModeButtonElement.classList.toggle('darkmode-icon--white');
 });
 
 //carga hasta el fin de la pagina
 
 // window.scroll({
-// 		top:99999999999,	
+// 		top:99999999999,
 // 		behavior:"smooth",
 // 	});
-		
